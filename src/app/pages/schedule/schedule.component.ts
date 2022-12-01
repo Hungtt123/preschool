9adb826736f954307d79e643ea26d9b9a3666544
Cong <cong.pd2279@sinhvien.hoasen.edu.vn>
2022-12-01 21:55:10 +0700

    cap nhat nav

--- src/app/pages/schedule/schedule.component.ts
+++ src/app/pages/schedule/schedule.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Auth,User } from '@angular/fire/auth';
+import * as AuthAction from "src/app/actions/auth.action"
+import { Store } from '@ngrx/store';
+import { AuthState } from 'src/app/states/auth.state';
+import { AuthService } from 'src/app/services/auth.service';
 
 @Component({
   selector: 'app-schedule',
@@ -7,7 +12,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ScheduleComponent implements OnInit {
 
-  constructor() { }
+  url = "https://inkythuatso.com/uploads/thumbnails/800/2022/05/hinh-nen-dien-thoai-tone-trang-tinh-khiet-1-19-10-27-56.jpg";
+  onselectFile(e: any) {
+    console.log(e);
+    if (e.target.files) {
+      var reader = new FileReader();
+      reader.readAsDataURL(e.target.files[0])
+      reader.onload = (event: any) => { this.url = event.target.result; }
+    }
+  }
+
+  onImageError(e: any) {
+    e.target.src = 'https://vnn-imgs-a1.vgcloud.vn/image1.ictnews.vn/_Files/2020/03/17/trend-avatar-1.jpg'
+  }
+
+  public user!: User;
+  public isLoad = true;
+  constructor(private store: Store<{ auth: AuthState }>, private AuthService: AuthService) {
+    this.AuthService.user$.subscribe(user => {
+      if (user.email) {
+        this.user = user;
+      }
+    })
+  }
+  login() {
+    this.store.dispatch(AuthAction.login())
+  }
+
+
+
+
+  public file!: any;
+  public imageSrc!: any;
+  public content!: string;
+
+
+  onSelect(event: any) {
+    if (event.target.files && event.target.files[0]) {
+      this.file = event.target.files[0];
+
+      const reader = new FileReader();
+      reader.onload = e => this.imageSrc = reader.result;
+
+      reader.readAsDataURL(this.file);
+    }
+  }
+
+  async signOut() {
+    await this.AuthService.logout();
+    window.location.href = "/"
+  }
+
 
   ngOnInit(): void {
   }
